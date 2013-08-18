fec9578a349e851e828ae21ab783fe47a6069e61
pglevy <philipglevy@gmail.com>
2013-08-17 23:06:05 -0400

    added easy/hard modes, refactored code, added hidden rows

--- js/main.js
+++ js/main.js
@@ -1,56 +1,151 @@
 $(document).ready(function() {
-	$('#checkAnswer').addClass('disabled');
-	$('#showAnswer').addClass('disabled');
-	//$('.alert').hide();
+	//global variables
+	var answerRounded;
+	var remainder;
+	var arraySize;
 
-	var r = new Array();
-  
-	$('#newProblem').click(function() {
-		$('input').val(null);
-		$('.alert').text('You can do it!');
-		//$('.alert').hide();
+	//set the mode
+	var mode = "easy";
+	console.log("mode = " + mode);
+	
+	if (mode == "hard") {
+		arraySize = 5;
+	} else {
+		arraySize = 4;
+		$('.hardCol').hide();
+	}
 
-		for (var i=1; i<4; i++) {
-			r[i] = Math.floor(Math.random()*10);
-			if ( r[1] == 0 || r[1] == 1) {
-				r[1] = 2;
-			}
-			if ( r[2] == 0) {
-				r[2] = 1;
-			}
-		}
-		$('#divisor').val(r[1]);
-		$('#dividend100').val(r[2]);
-		$('#dividend10').val(r[3]);
-		$('#dividend1').val(r[4]);
-
+	//disable check and show buttons when starting new problem
+	function disableButtons() {
 		$('#checkAnswer').addClass('disabled');
 		$('#showAnswer').addClass('disabled');
-		//alert(r[1] + ' ' + r[2] + ' ' + r[3] + ' ' + r[4]);
-		//alert(r);
-	});
+	}
 
+	//hide extra work rows
+	function hideRows() {
+		$('#workRow1').hide();
+		$('#workRow2').hide();
+		$('#workRow3').hide();
+		$('#workRow4').hide();
+		$('#workRow5').hide();
+		$('#workRow6').hide();
+	};
+
+	//reset everything
+	function resetEverything() {
+		$('input').val(null);
+		answerRounded = null;
+		remainder = null;
+		disableButtons();
+		hideRows();
+	}
+
+	//display answer rows as needed
+	$('#myAnswer1').focus(function() {
+		$('#workRow1').show();
+		$('#workRow2').show();
+	});
+	$('#myAnswer2').focus(function() {
+		$('#workRow3').show();
+		$('#workRow4').show();
+	});
+	$('#myAnswer3').focus(function() {
+		$('#workRow5').show();
+		$('#workRow6').show();
+	});
+	
+	//after answer has been checked, enable show button in case answer is wrong
 	$('input').blur(function() {
 		$('#checkAnswer').removeClass('disabled');
 	});
 
+	//select input when tapped for easier input
 	$('input').click(function() {
 	   $(this).select();
 	});
 
-	$('#checkAnswer').click(function() {
-		$('.alert').text('');
+	//set mode
+	$('.mode').click(function() {
+		//console.log($(this.id));
+		if ($(this.id).selector == "hard") {
+			mode = "hard";
+			$('.hardCol').show();
+		} else {
+			mode = "easy";
+			$('.hardCol').hide();
+		}		
+		console.log(mode);
+		resetEverything();	
+	});
 
-		var myAnswer100 = $('#answer100').val();
-		var myAnswer10 = $('#answer10').val();
-		var myAnswer = myAnswer100 + myAnswer10;
-		var myRemainder = $('#remainder').val();
+	//create array for dividend and divsors
+	var r = new Array();
+  
+	$('#newProblem').click(function() {
+		//clear all the inputs
+		$('input').val(null);
+		//update the alert
+		$('.alert').text('You can do it!');
+		//$('.alert').hide();
 
-		var dividend = r[2]*10 + r[3];
+		//add random numbers to the array
+		for (var i=1; i<arraySize; i++) {
+			r[i] = Math.floor(Math.random()*10);
+			//don't divide by zero or one
+			if ( r[1] == 0 || r[1] == 1) {
+				r[1] = 2;
+			}
+			//don't let first number of dividend be zero
+			if ( r[2] == 0) {
+				r[2] = 1;
+			}
+		}
+
+		//fill in the blanks
+		$('#divisor').val(r[1]);
+		$('#dividend1').val(r[2]);
+		$('#dividend2').val(r[3]);
+		$('#dividend3').val(r[4]);
+
+		//disable the buttons again
+		disableButtons();
+
+		//calculate the answer here so it's ready
+		if (mode == "hard") {
+			var dividend = r[2]*100 + r[3]*10 + r[4]
+		} else {
+			var dividend = r[2]*10 + r[3];
+		}
 		var divisor = r[1];
 		var answer = dividend / divisor;
-		var answerRounded = Math.floor(answer);
-		var remainder = dividend - (answerRounded * divisor);
+		answerRounded = Math.floor(answer);
+		remainder = dividend - (answerRounded * divisor);
+		console.log("answerRounded: " + answerRounded);
+		console.log("remainder: " + remainder);
+
+		//alert(r[1] + ' ' + r[2] + ' ' + r[3] + ' ' + r[4]);
+		//alert(r);
+	});
+
+	$('#checkAnswer').click(function() {
+		//clear the alert
+		$('.alert').text('');
+
+		//calculate my answer
+		var myAnswer1 = $('#myAnswer1').val();
+		console.log(myAnswer1);
+		var myAnswer2 = $('#myAnswer2').val();
+		console.log(myAnswer2);
+		var myAnswer3 = $('#myAnswer3').val();
+		console.log(myAnswer3);
+		if (mode == "hard") {
+			var myAnswer = myAnswer1 + myAnswer2 + myAnswer3;
+		} else {
+			var myAnswer = myAnswer1 + myAnswer2;
+		}
+		var myRemainder = $('#remainder').val();
+		console.log("my answer: " + myAnswer);
+		console.log("my remainder: " + myRemainder);
 
 		if ( myAnswer == answerRounded && myRemainder == remainder ) {
 			console.log("correct!");
@@ -65,33 +160,40 @@ $(document).ready(function() {
 	})
 
 	$('#showAnswer').click(function() {
-		var dividend = r[2]*10 + r[3];
-		var divisor = r[1];
-		var answer = dividend / divisor;
-		var answerRounded = Math.floor(answer);
-		var remainder = dividend - (answerRounded * divisor);
-
 		var answerText = answerRounded.toString();
 		console.log(answerText);
-		var answer100 = answerText.substring(0, 1);
-		console.log(answer100);
-		console.log(answer10);
-		var answer10 = answerText.substring(1, 2);
-
-		if ( answerRounded < 10) {
-			$('#answer10').val(answer100);
+		if (mode == "hard") {
+			var answer1 = answerText.substring(0, 1);
+			var answer2 = answerText.substring(1, 2);
+			var answer3 = answerText.substring(2, 3);
+			//shift answer to correct position if less than 10 or 100
+			if (answerRounded < 10) {
+				$('#myAnswer3').val(answer1);
+			} else  if (answerRounded < 100) {
+				$('#myAnswer2').val(answer1);
+				$('#myAnswer3').val(answer2);
+			} else {
+				$('#myAnswer1').val(answer1);
+				$('#myAnswer2').val(answer2);
+				$('#myAnswer3').val(answer3);
+			}
 		} else {
-			$('#answer100').val(answer100);
-			$('#answer10').val(answer10);
+			var answer1 = answerText.substring(0, 1);
+			var answer2 = answerText.substring(1, 2);
+			//shift answer to correct position if less than 10 or 100
+			if (answerRounded < 10) {
+				$('#myAnswer2').val(answer1);
+			} else {
+				$('#myAnswer1').val(answer1);
+				$('#myAnswer2').val(answer2);
+			}
 		}
+
+		//if the remainder is greater than zero, show it
 		if ( remainder > 0 ) {
 			$('#remainder').val(remainder);
 		}
-		
-
-		//$('.alert').append(answer);
-		//$('.alert').show();
-		//alert(answer);
 	});
 
+	resetEverything();
 });
